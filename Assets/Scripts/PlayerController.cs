555a8e589c9ed19eaa7a011f078fc743f66890ea
IRKhabibullin <36387914+IRKhabibullin@users.noreply.github.com>
2022-02-02 20:42:37 +0300

    death zone

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public NavMeshAgent agent;
     public Transform destination;
+    public LayerMask deathZoneLayer;
+    public Material defaultMaterial;
+    public Material shieldMaterial;
+    private bool shieldEnabled;
+    private IEnumerator shieldCoroutine;
 
     void Start()
     {
@@ -17,5 +21,39 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(2);
         agent.SetDestination(destination.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((deathZoneLayer & (1 << other.transform.gameObject.layer)) != 0)
+        {
+            if (!shieldEnabled)
+                Debug.Log("Death");
+        }
+    }
+
+    public void EnableShield()
+    {
+        shieldCoroutine = ShieldCoroutine();
+        StartCoroutine(shieldCoroutine);
+    }
+
+    public IEnumerator ShieldCoroutine()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = shieldMaterial;
+        shieldEnabled = true;
+        yield return new WaitForSeconds(2);
+        gameObject.GetComponent<MeshRenderer>().material = defaultMaterial;
+        shieldEnabled = false;
+    }
+
+    public void DisableShield()
+    {
+        shieldEnabled = false;
+        gameObject.GetComponent<MeshRenderer>().material = defaultMaterial;
+        if (shieldCoroutine != null)
+        {
+            StopCoroutine(shieldCoroutine);
+        }
     }
 }
